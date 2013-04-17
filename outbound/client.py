bf7d073f7044ee8686e65122f07b9cf9ea4c3278
Michael Van Veen <michael@mvanveen.net>
2013-04-17 09:06:32 +0200

    initial gae build

--- outbound/client.py
+++ outbound/client.py
@@ -1,22 +1,37 @@
+import os
 import json
-import requests
+
+try:
+  import requests
+except:
+  pass
 
 API_SERVER = "http://api.outbound.io"
 IDENTIFY = "api/v1/identify"
 TRACK = "api/v1/track"
 
+
 class Client(object):
   def __init__(self, api_key):
     self._api_key = api_key
 
-  def identify(self, user_id, traits={}):
-    resp = requests.post("%s/%s" % (API_SERVER, IDENTIFY), data=json.dumps({
-      "api_key": self._api_key,
-      "user_id": user_id,
-      "traits": traits
-    }), headers={
-      "Content-Type": "application/json"
-    })
+  def fetch_response(self, url, payload):
+    return requests.post(
+      url,
+      data=payload,
+      headers={'Content-Type': 'application/json'}
+    )
+
+  def identify(self, user_id, traits={}, with_gae=False):
+    fetch = self.fetch_response if not with_gae else self.fetch_gae_response
+
+    resp = fetch(
+      "%s/%s" % (API_SERVER, IDENTIFY), json.dumps({
+        "api_key": self._api_key,
+        "user_id": user_id,
+        "traits": traits
+      }))
+
     if not resp.status_code == 200:
       return False
     else:
@@ -35,3 +50,16 @@ class Client(object):
       return False
     else:
       return resp.content
+
+
+class GAEClient(Client):
+  """Google App Engine-specific client"""
+  def fetch_response(self, url, payload):
+    return urlfetch.fetch(
+      url,
+      data=payload,
+      method=urlfetch.POST,
+      headers={
+        "Content-Type": "application/json"
+      })
+
